248c86c50b41e941bcb822749e0e7de4955b600c
Kateryna Peikova <peikovakate@gmail.com>
2018-04-29 14:42:32 +0300

    added pos tagger some conll reader fixing udp_yara formating

---
+++ resources/pos_tagger.py
@@ -0,0 +1,73 @@
+from resources.read_conll import ConllReader
+
+class POSTagger:
+    def __init__(self, model_file='../ukr/pos_model'):
+        self.dictionary = {}
+        self.model_file = model_file
+        self.load_model()
+
+    def load_model(self):
+        self.dictionary.clear()
+        file = open(self.model_file, 'r', encoding='utf-8')
+        lines = file.readlines()
+        for line in lines:
+            attributes = line.split('\t')
+            attributes[len(attributes)-1] = attributes[len(attributes)-1][:-1]
+            word = attributes[0]
+            self.dictionary[word]={}
+            for attr in attributes[1:]:
+                pos, freq = attr.split('_')
+                self.dictionary[word][pos] = int(freq)
+        file.close()
+
+    def pos_tags(self, word):
+        word.lower()
+        if word in self.dictionary:
+            return self.dictionary[word]
+        else:
+            return {}
+
+    def most_freq_tag(self, word):
+        frequencies = self.pos_tags(word)
+        max_freq = 0
+        m_tag = ''  # tag that has maximum frequency
+        for tag in frequencies:
+            if max_freq < frequencies[tag]:
+                m_tag = tag
+                max_freq = frequencies[tag]
+        return m_tag
+
+
+    @staticmethod
+    def train(file_name, file_output):
+        '''takes conll file'''
+        conll_tagged = ConllReader.read_as_raw(file_name)
+        dict = {}
+        for raw in conll_tagged:
+            word = raw[1].lower()
+            pos_tag = raw[3]
+            if word in dict:
+                if pos_tag in dict[word]:
+                    dict[word][pos_tag] += 1
+                else:
+                    dict[word][pos_tag] = 1
+            else:
+                dict[word] = {}
+                dict[word][pos_tag] = 1
+        POSTagger.write_trained(file_output, dict)
+
+    @staticmethod
+    def write_trained(file_output, trained_dictionary):
+        file = open(file_output, 'w', encoding='utf-8')
+        for word in trained_dictionary:
+            string = word + '\t'
+            string += '\t'.join(key+'_'+str(trained_dictionary[word][key]) for key in trained_dictionary[word])
+            string += '\n'
+            file.write(string)
+        file.close()
+
+
+
+# POSTagger.train('../ukr/train.conll', '../ukr/pos_model')
+tagger = POSTagger()
+print(tagger.most_freq_tag('у'))
--- resources/read_conll.py
+++ resources/read_conll.py
@@ -1,4 +1,3 @@
-import  networkx as nx
 class ConllReader:
     # todo: check for multiple trees from parser
     @staticmethod
@@ -13,7 +12,7 @@ class ConllReader:
             if line == '\n':
                 trees.append(ConllReader.split_tree(lines_of_tree))
                 lines_of_tree.clear()
-            else:
+            elif line[0] != '#':
                 lines_of_tree.append(line)
         # if we got to the end of files, but the lust sent still in buffer of lines_of_tree
         if len(lines_of_tree) != 0:
@@ -22,16 +21,31 @@ class ConllReader:
         return trees
 
     @staticmethod
+    def read_as_raw(file_name):
+        raws = []
+        file = open(file_name, "r", encoding='utf-8')
+        lines = file.readlines()
+        file.close()
+        for line in lines:
+            if line != '\n' and line[0] != '#':
+                raws.append(ConllReader.split_line(line))
+        return raws
+
+    @staticmethod
+    def split_line(line):
+        attributes = line.split("\t")
+        # for last attribute there is an additional '\n' sign - end of line, that we get reed of
+        attributes[len(attributes) - 1] = attributes[len(attributes) - 1][:-1]
+        return attributes
+
+    @staticmethod
     def split_tree(lines):
         '''takes list of lines that refers to one sentence,
         returns list of description for every word,
         description = list of attributes'''
         tree = []
         for line in lines:
-            attributes = line.split("\t")
-            # for last attribute there is an additional '\n' sign - end of line, that we get reed of
-            attributes[len(attributes)-1] = attributes[len(attributes)-1][:-1]
-            tree.append(attributes)
+            tree.append(ConllReader.split_line(line))
         return tree
 
 
